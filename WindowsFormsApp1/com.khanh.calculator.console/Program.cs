d22bc0abf2f2be695025ed0aecd96fa8c9709594
Ho Quoc Khanh (FSU1.DES) <KhanhHQ1@fsoft.com.vn>
2018-09-05 14:53:54 +0700

    frist commit

---
+++ WindowsFormsApp1/com.khanh.calculator.console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using com.khanh.calculator.operation;
+
+namespace com.khanh.calculator.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write(CalculatorOperation.DevideTwoNumber(1, 0));
+            }
+            catch (Exception ex) {
+                Console.Write(ex.Message);
+
+            }
+            Console.ReadKey();
+        
+        }
+    }
+}
---
+++ WindowsFormsApp1/com.khanh.calculator.operation/CalculatorException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.khanh.calculator.operation
+{
+    class CalculatorException:Exception
+    {
+       
+        public CalculatorException(string message):base(message)
+        {
+            
+        }
+    }
+}
